216e5384c6ba1f6db21dbb1aee64136cfd095c7f
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2026-05-27 20:52:06 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -262,7 +262,7 @@ EOF
 sysctl --system
 fi
 
-cat >> /root/.bashrc <<'EOF'
+sudo sh -c "cat >> /root/.bashrc <<'EOF'
 
 if [ \"\$USER\" = \"root\" ]; then
     PS1='\${debian_chroot:+(\$debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]# '
